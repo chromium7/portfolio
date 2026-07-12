f61e158e5c6fa8954b05b1abe4af09b620fedf79
chromium7 <christofersaputra29@gmail.com>
2026-07-12 19:03:31 +0700

    Move to static files dir

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -21,11 +22,11 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('markdownx/', include('markdownx.urls')),
-    path('api/', include('portfolio.api.urls', namespace='api')),
-    path('blog/', include('portfolio.apps.blogs.urls', namespace='blogs')),
-    path('', include('portfolio.apps.pages.urls', namespace='pages')),
+    path("admin/", admin.site.urls),
+    path("markdownx/", include("markdownx.urls")),
+    path("api/", include("portfolio.api.urls", namespace="api")),
+    path("blog/", include("portfolio.apps.blogs.urls", namespace="blogs")),
+    path("", include("portfolio.apps.pages.urls", namespace="pages")),
 ]
 
 if settings.DEBUG:
@@ -33,8 +34,7 @@ if settings.DEBUG:
 
     try:
         import debug_toolbar
-        urlpatterns += [
-            path('__debug__/', include(debug_toolbar.urls))
-        ]
+
+        urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
     except ImportError:
         pass
